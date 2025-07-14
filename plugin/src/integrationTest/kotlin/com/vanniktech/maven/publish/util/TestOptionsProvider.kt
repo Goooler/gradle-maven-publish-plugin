c67de1c65e88e16d1165ca635432aff817456f6d
Goooler <wangzongler@gmail.com>
2025-07-14 09:40:10 +0800

    No need to use `ImmutableList`

--- plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/util/TestOptionsProvider.kt
+++ plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/util/TestOptionsProvider.kt
@@ -1,6 +1,5 @@
 package com.vanniktech.maven.publish.util
 
-import com.google.common.collect.ImmutableList
 import com.google.testing.junit.testparameterinjector.junit5.TestParameterValuesProvider
 
 private val quickTestProperty get() = System.getProperty("quickTest")
@@ -9,47 +8,47 @@ internal class TestOptionsConfigProvider : TestParameterValuesProvider() {
   override fun provideValues(context: Context?): List<*> {
     val property = System.getProperty("testConfigMethod")
     if (property.isNotBlank()) {
-      return ImmutableList.of(TestOptions.Config.valueOf(property))
+      return listOf(TestOptions.Config.valueOf(property))
     }
     if (quickTestProperty.isNotBlank()) {
-      return ImmutableList.of(TestOptions.Config.BASE)
+      return listOf(TestOptions.Config.BASE)
     }
-    return ImmutableList.copyOf(TestOptions.Config.values())
+    return TestOptions.Config.values().toList()
   }
 }
 
 internal class GradleVersionProvider : TestParameterValuesProvider() {
   override fun provideValues(context: Context?): List<*> {
     if (quickTestProperty.isNotBlank()) {
-      return ImmutableList.of(GradleVersion.values().last())
+      return listOf(GradleVersion.values().last())
     }
-    return ImmutableList.copyOf(GradleVersion.values().distinctBy { it.value })
+    return GradleVersion.values().distinctBy { it.value }
   }
 }
 
 internal class AgpVersionProvider : TestParameterValuesProvider() {
   override fun provideValues(context: Context?): List<*> {
     if (quickTestProperty.isNotBlank()) {
-      return ImmutableList.of(AgpVersion.values().last())
+      return listOf(AgpVersion.values().last())
     }
-    return ImmutableList.copyOf(AgpVersion.values().distinctBy { it.value })
+    return AgpVersion.values().distinctBy { it.value }
   }
 }
 
 internal class KotlinVersionProvider : TestParameterValuesProvider() {
   override fun provideValues(context: Context?): List<*> {
     if (quickTestProperty.isNotBlank()) {
-      return ImmutableList.of(KotlinVersion.values().last())
+      return listOf(KotlinVersion.values().last())
     }
-    return ImmutableList.copyOf(KotlinVersion.values().distinctBy { it.value })
+    return KotlinVersion.values().distinctBy { it.value }
   }
 }
 
 internal class GradlePluginPublishVersionProvider : TestParameterValuesProvider() {
   override fun provideValues(context: Context?): List<*> {
     if (quickTestProperty.isNotBlank()) {
-      return ImmutableList.of(GradlePluginPublish.values().last())
+      return listOf(GradlePluginPublish.values().last())
     }
-    return ImmutableList.copyOf(GradlePluginPublish.values().distinctBy { it.version })
+    return GradlePluginPublish.values().distinctBy { it.version }
   }
 }
